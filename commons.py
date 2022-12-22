cd8b880f76e5418d6701f77ee65c2fd7e0e1c725
lc787 <lucacostin01@gmail.com>
2022-12-22 08:42:33 +0200

    - changed theme structure to use a dict instead of a class - added key binds

--- commons.py
+++ commons.py
@@ -1,28 +1,13 @@
-LOGIC_OBJECTS = []
-# Draw order is maintained by the draw_level property
-CANVAS_OBJECTS = []
+import pygame as pg
+from gameengine import Key
 
 width = 800
 height = 600
 target_fps = 60
 game_running = False
 
-class ColorTheme:
-    def __init__(self, name, colors: dict):
-        self.name = name
-        self.block_cyan = colors['block_cyan']
-        self.block_blue = colors['block_blue']
-        self.block_orange = colors['block_orange']
-        self.block_yellow = colors['block_yellow']
-        self.block_green = colors['block_green']
-        self.block_purple = colors['block_purple']
-        self.block_red = colors['block_red']
-        self.block_grey = colors['block_grey']
-        self.empty = colors['empty']
-        self.background = colors['background']
 
-
-color_theme = ColorTheme("default", {
+color_theme_default = {'name': "default", 'palette': {
     "block_cyan": (0, 255, 255),
     "block_blue": (0, 0, 255),
     "block_orange": (255, 165, 0),
@@ -33,17 +18,15 @@ color_theme = ColorTheme("default", {
     "block_grey": (128, 128, 128),
     "empty": (255, 255, 255),
     "background": (120, 120, 120)
-})
+}}
 
-color_theme2 = ColorTheme("dark", {
-    "block_cyan": (255, 165, 0),
-    "block_blue": (0, 255, 0),
-    "block_orange": (0, 255, 255),
-    "block_yellow": (255, 255, 0),
-    "block_green": (0, 0, 255),
-    "block_purple": (255, 0, 0),
-    "block_red": (128, 0, 128),
-    "block_grey": (128, 128, 128),
-    "empty": (255, 255, 255),
-    "background": (0, 0, 0)
-})
+key_binds = {
+    'clockwise': Key(pg.K_e),
+    'counter_clockwise': Key(pg.K_q),
+    'switch': Key(pg.K_w),
+    'hard_drop': Key(pg.K_SPACE),
+    'soft_drop': Key(pg.K_DOWN),
+    'left': Key(pg.K_LEFT),
+    'right': Key(pg.K_RIGHT),
+    '180': Key(pg.K_p),
+}
